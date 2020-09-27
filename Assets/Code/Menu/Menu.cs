9a1cddc6669e6bf9b022d2b9b8d9105431709058
FEckeroth <46935554+FEckeroth@users.noreply.github.com>
2020-09-27 21:45:38 +0200

    Menu almost done

---
+++ Assets/Code/Menu/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject pauseMenu, optionsMenu;
+
+    public GameObject pauseResumeButton, optionButton, mainMenuButton, quitToDesktopButton;
+
+    public GameObject customKeybindingButton, masterVolumeButton, soundEffectsButton, musicVolumeButton, backButton;
+
+   
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Fire3"))
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        if (!pauseMenu.activeInHierarchy)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+
+            //Clear seletion
+            EventSystem.current.SetSelectedGameObject(null);
+
+            EventSystem.current.SetSelectedGameObject(pauseResumeButton);
+
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            optionsMenu.SetActive(false);
+        }
+
+    }
+
+    public void OpenOptions()
+
+    {
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        //Clears selection
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(customKeybindingButton);
+    }
+    public void CloseOptions()
+    {
+        optionsMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        //Clears selection
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(pauseResumeButton);
+    }
+
+
+   
+}
