2f6f8a8caa01d1e767c1abf2a70ff131f9c0891c
FEckeroth <46935554+FEckeroth@users.noreply.github.com>
2020-09-28 17:48:03 +0200

    Done

--- Assets/Code/Menu/Menu.cs
+++ Assets/Code/Menu/Menu.cs
@@ -6,17 +6,19 @@ using UnityEngine.EventSystems;
 public class Menu : MonoBehaviour
 {
 
-    public GameObject pauseMenu, optionsMenu;
+    public GameObject pauseMenu, optionsMenu, mainMenu;
 
     public GameObject pauseResumeButton, optionButton, mainMenuButton, quitToDesktopButton;
 
     public GameObject customKeybindingButton, masterVolumeButton, soundEffectsButton, musicVolumeButton, backButton;
 
-   
+
 
     // Start is called before the first frame update
     void Update()
     {
+
+
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Fire3"))
         {
             PauseUnpause();
@@ -27,7 +29,10 @@ public class Menu : MonoBehaviour
     {
         if (!pauseMenu.activeInHierarchy)
         {
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(false);
             pauseMenu.SetActive(true);
+
             Time.timeScale = 0f;
 
             //Clear seletion
@@ -39,8 +44,10 @@ public class Menu : MonoBehaviour
         else
         {
             pauseMenu.SetActive(false);
+            //mainMenu.SetActive(false);
+            //optionsMenu.SetActive(false);
             Time.timeScale = 1f;
-            optionsMenu.SetActive(false);
+
         }
 
     }
@@ -48,6 +55,7 @@ public class Menu : MonoBehaviour
     public void OpenOptions()
 
     {
+        mainMenu.SetActive(false);
         pauseMenu.SetActive(false);
         optionsMenu.SetActive(true);
         //Clears selection
@@ -57,14 +65,33 @@ public class Menu : MonoBehaviour
     }
     public void CloseOptions()
     {
-        optionsMenu.SetActive(false);
         pauseMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(false);
         //Clears selection
         EventSystem.current.SetSelectedGameObject(null);
 
         EventSystem.current.SetSelectedGameObject(pauseResumeButton);
     }
 
+    public void MainMenuButton()
+    {
 
-   
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+
+    }
+
+    public void ResumeButton()
+    {
+        PauseUnpause();
+    
+    
+    }
+    public void BackButton()
+    {
+        PauseUnpause();
+
+    }
 }
