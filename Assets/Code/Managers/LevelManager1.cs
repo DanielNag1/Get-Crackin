657aec3d3a39d93e8162d969689326a788014f93
EmmaAndersen <48715807+EmmaAndersen@users.noreply.github.com>
2020-10-03 17:14:21 +0200

    Added dynamic updating of NavMesh
    Have to polish the spawnpoints so they line with the ground. The transform values are a bit wierd.

---
+++ Assets/Code/Managers/LevelManager1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelManager1 : MonoBehaviour
+{
+    #region Variables
+
+    public NavMeshSurface navMeshSurface;
+
+    #endregion
+
+    void Start()
+    {
+        navMeshSurface.BuildNavMesh();
+    }
+}
