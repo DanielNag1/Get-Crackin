234088a25517db94bb981435b36fe8f65f413541
EmmaAndersen <48715807+EmmaAndersen@users.noreply.github.com>
2020-10-19 18:04:45 +0200

    Added raycast instead of using Distance with enemy
    Only tested on 1 active enemy

--- Assets/Code/Enemies/EnemyOne.cs
+++ Assets/Code/Enemies/EnemyOne.cs
@@ -9,6 +9,11 @@ public class EnemyOne : MonoBehaviour
     private FiniteStateMachine _finiteStateMachine;
     public GameObject _player;
 
+    public int fieldOfView = 500;
+    public int viewDistance = 10;
+
+    private Vector3 rayDirection;
+
     private void Awake()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
@@ -31,10 +36,45 @@ public class EnemyOne : MonoBehaviour
         _finiteStateMachine.AddAnyTransition(attack, AttackTarget());
 
 
-        Func<bool> HasATarget() => () => IsWithinChaseDistance();
+        Func<bool> HasATarget() => () => Detect();
 
         Func<bool> AttackTarget() => () => IsWithinAttackDistance();
 
+        Detect();
+
+    }
+
+    private bool Detect()
+    {
+        RaycastHit hit;
+        rayDirection = _player.transform.position - this.transform.position;
+        if ((Vector3.Angle(rayDirection, this.transform.forward)) < fieldOfView)
+        {
+            if(Physics.Raycast(this.transform.position, rayDirection, out hit, viewDistance))
+            {
+                Debug.Log("Detech Hit");
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(_player.transform == null)
+        {
+            return;
+        }
+
+        Debug.DrawLine(transform.position, _player.transform.position, Color.red);
+
+        Vector3 frontRayPoint = transform.position + (transform.forward * viewDistance);
+
+        Debug.DrawLine(transform.position, frontRayPoint, Color.blue);
     }
 
     #region Conditions
@@ -53,7 +93,7 @@ public class EnemyOne : MonoBehaviour
 
     private bool IsWithinAttackDistance()
     {
-        if(Vector3.Distance(this.transform.position, _player.transform.position) <= 1)
+        if (Vector3.Distance(this.transform.position, _player.transform.position) <= 1)
         {
             return true;
         }
