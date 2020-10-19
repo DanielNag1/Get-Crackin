0174e79323668b36e896392045eab908b6963dff
Broman <erik.broman@live.se>
2020-10-19 10:47:08 +0200

    grav + basic save

---
+++ Assets/Code/Managers/SaveFolder/SaveManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveManager : MonoBehaviour
+{
+    public static void Save()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/save.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        SaveData data = new SaveData();
+    }
+}
