6eb8ef938a537f5a5a6b2e5c094bac22658b8b71
DanielNag1 <daniel.nagy11@gmail.com>
2020-11-16 13:22:30 +0100

    La in banan i Sandra PCG scenen och skapat ett script

---
+++ Assets/Code/PCG/TerrainTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
+
+public class TerrainTest : MonoBehaviour
+{
+    public TerrainLayer assda;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(assda.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
