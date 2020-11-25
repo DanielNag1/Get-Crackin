70348a4fe58a1a5e5aba8db552affba9cf00f284
DanielNag1 <daniel.nagy11@gmail.com>
2020-11-25 15:50:11 +0100

    Mördade uppgiften och är 90% färdig, väntar på steves bedömning över våra liv.

--- Assets/Code/PCG/CreateGrid.cs
+++ Assets/Code/PCG/CreateGrid.cs
@@ -6,35 +6,31 @@ public class CreateGrid : MonoBehaviour
 {
     public float[,] Grid;
     private float Vertical, Horizontal;
-    [Range(1, 30)]
-    public float Columns, Rows;
+    [Range(1, 500)]
+    public float Columns, Rows, seed;
+    [Range(1, 10)]
+    public float noiseScale;
     [Range(1, 50)]
     public int horizontalFrequence, verticalFrequence;
     public TerrainData terrainData;
     public Sprite picture;
-    public List<GameObject> losListos;
+    public List<GameObject> positionList;
+
     void Start()
     {
-        losListos = new List<GameObject>();
+        positionList = new List<GameObject>();
         Horizontal = terrainData.size.x;
         Vertical = terrainData.size.z;
     }
 
-    void Update()
+    public void SpawnGrid()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        foreach (var g in positionList)
         {
-            SpawnGrid();
+            DestroyImmediate(g);
         }
-    }
+        //positionList.Clear();
 
-    void SpawnGrid()
-    {
-        foreach (var g in losListos)
-        {
-            Destroy(g);
-        }
-        losListos.Clear();
         Grid = new float[(int)Columns, (int)Rows];
 
         for (int i = 0; i < Columns; i++)
@@ -45,17 +41,36 @@ public class CreateGrid : MonoBehaviour
             }
         }
     }
+
+    Vector3 ApplyNoise(float x, float y)
+    {
+        float xCoord = seed * x / Columns;
+        float yCoord = seed * y / Rows;
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Vector3(x * sample * noiseScale, 100, y * sample * noiseScale);
+    }
+
     void SpawnTile(int x, int y)
     {
-        if (x < Horizontal && y < Vertical)
+        if (x < Horizontal && y < Vertical && x > 0 && y > 0) //Tar bort från grid om den är utanför banan
         {
             GameObject g = new GameObject("X: " + x + "Y: " + y);
             var s = g.AddComponent<SpriteRenderer>();
             s.sprite = picture;
-            g.transform.position = new Vector3(x, 100, y);
-            g.transform.localScale = new Vector3(100, 100, 100);
-            g.transform.Rotate(90, 0, 0);
-            losListos.Add(g);
+            //g.transform.position = new Vector3(x, 100, y);
+
+            g.transform.position = ApplyNoise(x, y);
+
+            if (g.transform.position.x < Horizontal && g.transform.position.z < Vertical
+                && g.transform.position.x > 0 && g.transform.position.z > 0) //Tar bort från noise om den är utanför banan
+            {
+                g.transform.Rotate(90, 0, 0);
+                positionList.Add(g);
+            }
+            else
+            {
+                DestroyImmediate(g);
+            }
         }
     }
 }
---
+++ Assets/Code/PCG/ReadTerrain.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
+using UnityEditor;
+
+public class ReadTerrain : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    private Terrain terrain;
+    private TerrainData terrainData;
+    private Vector3 terrainPos;
+
+    public float[] textureValues;
+    public CreateGrid grid;
+
+    public GameObject objectToSpawn;
+    public string fileName;
+
+    private List<GameObject> spawnedObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = Terrain.activeTerrain;
+        terrainData = terrain.terrainData;
+        terrainPos = terrain.transform.position;
+        spawnedObjects = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+
+
+    //Physics.Raycast(test.transform.position, Vector3.down, out hit, 500, 1);
+    //Debug.DrawRay(test.transform.position, Vector3.down * 500, Color.red);
+
+    private void GetTextureMix(Vector3 WorldPos)
+    {
+        // returns an array containing the relative mix of textures
+        // on the main terrain at this world position.
+
+        // The number of values in the array will equal the number
+        // of textures added to the terrain.
+
+        // calculate which splat map cell the worldPos falls within (ignoring y)
+        int mapX = (int)(((WorldPos.x - terrainPos.x) / terrainData.size.x) * terrainData.alphamapWidth);
+        int mapZ = (int)(((WorldPos.z - terrainPos.z) / terrainData.size.z) * terrainData.alphamapHeight);
+
+        // get the splat data for this cell as a 1x1xN 3d array (where N = number of textures)
+        float[,,] splatmapData = terrainData.GetAlphamaps(mapX, mapZ, 1, 1);
+
+        textureValues[0] = splatmapData[0, 0, 0];
+        textureValues[1] = splatmapData[0, 0, 1];
+        //textureValues[2] = splatmapData[0, 0, 2];
+
+    }
+
+    private void CreateTree(Vector3 position, Quaternion rotation)
+    {
+        float scale = Random.Range(1, 2.5f);
+        GameObject spawnedObject = Instantiate(objectToSpawn, position, rotation, transform);
+        spawnedObject.transform.localScale = new Vector3(scale, scale, scale);
+        spawnedObjects.Add(spawnedObject);
+    }
+
+    public void SpawnTrees()
+    {
+        grid.SpawnGrid(); //Spawnar en grid
+
+        foreach (var g in spawnedObjects) //Tar bort spawnpoints
+        {
+            DestroyImmediate(g);
+        }
+        spawnedObjects.Clear(); //Tar bort spawnpoints listan
+
+        foreach (var g in grid.positionList) //Gör en raycast från varje spawnpoint och antingen skapar ett träd på hit punkten eller tar bort spawnpointen
+        {
+            Physics.Raycast(g.transform.position, Vector3.down, out hit, 500, 1);
+            //Debug.DrawRay(g.transform.position, Vector3.down * 500, Color.red);
+
+            if (hit.point != null)
+            {
+                GetTextureMix(hit.point);
+
+                if (textureValues[1] > 0) //Snow är större än 0
+                {
+                    DestroyImmediate(g);
+                }
+                else
+                {
+                    CreateTree(hit.point, hit.transform.rotation); //Placerar ut ett träd på hit punkten.
+                }
+            }
+        }
+
+        foreach (var g in grid.positionList)
+        {
+            DestroyImmediate(g);
+        }
+
+        grid.positionList.Clear();
+    }
+    public void SaveTrees()
+    {
+        string localPath = "Assets/Prefabs/Maps/ " + fileName + ".prefab";
+        localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+        PrefabUtility.SaveAsPrefabAssetAndConnect(gameObject, localPath, InteractionMode.UserAction);
+    }
+
+}
