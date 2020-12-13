c0827d5a7d0778bb68b9f62a78cd538ce7855e4e
babyeatermcdeath <49117966+babyeatermcdeath@users.noreply.github.com>
2020-12-13 13:49:28 +0100

    Fixed menu transitions
    Fade in/out fixed

--- Assets/Code/Menu/LoadLevel.cs
+++ Assets/Code/Menu/LoadLevel.cs
@@ -9,13 +9,13 @@ public class LoadLevel : MonoBehaviour
     public Animator animator;
     public float transitionTime = 1f;
 
-    // Start is called before the first frame update
-    void Start()
+    
+    void Awake()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
@@ -23,13 +23,12 @@ public class LoadLevel : MonoBehaviour
 
     public void LoadNextLevel()
     {
+        animator.SetTrigger("Start");
         StartCoroutine(LoadNewLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
     IEnumerator LoadNewLevel(int levelIndex)
     {
-        animator.SetTrigger("Start");
-
         yield return new WaitForSeconds(transitionTime);
 
         SceneManager.LoadSceneAsync(levelIndex);
