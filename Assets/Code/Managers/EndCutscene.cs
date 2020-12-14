5dcb9c0cfdaa7b4caa8497f84f06b3e7aea8c123
DanielNag1 <daniel.nagy11@gmail.com>
2020-12-14 17:31:24 +0100

    Created a previz of the cutscene

---
+++ Assets/Code/Managers/EndCutscene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class EndCutscene : MonoBehaviour
+{
+    #region Variables
+    private PlayableDirector director;
+    #endregion
+    #region Methods
+
+    private void Awake()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+    private void Update()
+    {
+        if (director.state != PlayState.Playing)
+        {
+            EndScene();
+        }
+    }
+
+    private void EndScene()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+    #endregion
+}
